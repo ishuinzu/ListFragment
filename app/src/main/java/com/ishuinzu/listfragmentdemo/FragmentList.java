1e356b181acb848080abad442dda9540834e3983
Muhammad Inzmam <ishuinzu@gmail.com>
2021-05-03 17:19:23 +0500

    Initial commit

---
+++ app/src/main/java/com/ishuinzu/listfragmentdemo/FragmentList.java
@@ -0,0 +1,41 @@
+package com.ishuinzu.listfragmentdemo;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.ListFragment;
+
+public class FragmentList extends ListFragment implements AdapterView.OnItemClickListener {
+
+    public FragmentList() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.list_fragment, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        //Adapter
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getActivity(), R.array.Users, android.R.layout.simple_list_item_1);
+        setListAdapter(arrayAdapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        Toast.makeText(getActivity(), "Position : " + position, Toast.LENGTH_SHORT).show();
+    }
+}
